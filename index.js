41fc71f38c8f20ef0d03b1339c91553dc6459cf1
Habi <habi.thapa@gmail.com>
2024-06-24 02:06:52 +0545

    get product by Id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model");
 const app = express();
 
 app.use(express.json());
@@ -8,9 +9,32 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API what up");
 });
 
-app.post("/api/products", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 mongoose
